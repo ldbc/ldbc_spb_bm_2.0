dfa1ea0321d2936500c24fa008d8a2e9743d882e
Mitko Iliev <imitko@openlinksw.com>
2015-03-18 17:13:48 +0200

    Added shell script for initial setup of the Virtuoso DB

---
+++ datasets_and_queries/scripts/load_virt.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]
+then
+    echo "Usage: load_virt.sh <port> <SPB home directory>"
+    exit
+fi
+
+PORT=${1-1111}
+SPB=${2-~/ldbc_spb_bm_2.0}
+TDIR=`pwd`
+
+if [ -z "$VIRTUOSO_HOME" ]
+then
+    echo "Should run this script under Virtuoso environment"
+    exit
+fi
+
+isql $PORT dba dba exec="ld_dir_all ('$SPB/dist/data/ontologies', '*.ttl', 'ldbc-onto')"
+isql $PORT dba dba exec="ld_dir_all ('$SPB/dist/data/datasets', '*.ttl', 'ldbc')"
+for i in {1..8}; do isql $PORT dba dba exec="rdf_loader_run()" & done
+wait
+isql $PORT dba dba exec="rdfs_rule_set ('ldbc', 'ldbc-onto')"
+cd $SPB/dist
+java -jar semantic_publishing_benchmark-basic-virtuoso.jar test.properties 
+cd $TDIR
+isql $PORT dba dba exec="ld_dir_all ('$SPB/dist/generated', '*.nq', 'ldbc')"
+for i in {1..16}; do isql $PORT dba dba exec="rdf_loader_run()" & done
+isql $PORT dba dba exec="VT_INC_INDEX_DB_DBA_RDF_OBJ()" &
+isql $PORT dba dba exec="rdf_geo_fill ()"
+wait
+isql $PORT dba dba exec="checkpoint"
