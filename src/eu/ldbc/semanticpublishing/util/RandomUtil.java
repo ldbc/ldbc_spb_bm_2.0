5da2fd48dc13adc2789b48212ce47557714ac14e
venkot <venelin.kotsev@ontotext.com>
2015-03-24 17:00:38 +0200

    Fixed missing imports

--- src/eu/ldbc/semanticpublishing/util/RandomUtil.java
+++ src/eu/ldbc/semanticpublishing/util/RandomUtil.java
@@ -1,8 +1,10 @@
 package eu.ldbc.semanticpublishing.util;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
